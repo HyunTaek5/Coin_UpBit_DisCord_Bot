581523c6e33a9241e462608e0ce5d73870969a42
HyunTaek5 <oht222@naver.com>
2021-04-17 21:23:13 +0900

    [update][main] open secret_file & get_secret() to core.py & bot command "!시세"

--- main.py
+++ main.py
@@ -1,27 +1,23 @@
-import os
-import json
 import discord
 from discord.ext import commands
-
-secret_file = os.path.join('secrets.json')
-with open(secret_file) as f:
-    secret = json.loads(f.read())
-
-
-def get_secret(setting, secrets=secret):
-    try:
-        return secrets[setting]
-    except KeyError:
-        print('error!')
-
+from core import get_secret
+from upBit import balance_check, get_trade_price
 
 token = get_secret("TOKEN_KEY")
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot = commands.Bot(command_prefix='>')
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} 에 로그인하였습니다!")
+
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
-
+async def 시세(ctx, arg):
+    try:
+        await ctx.reply(f'{get_trade_price(arg)} KRW')
+    except KeyError:
+        await ctx.reply('다시 입력하십시오')
 
 bot.run(token)
