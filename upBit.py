3cc17a7d6376c26b1491253acbd5074c5b065eda
HyunTaek5 <oht222@naver.com>
2021-04-17 21:22:06 +0900

    [upload][upBit] create balance_check() & get_trade_price() & get_all_coiin()

---
+++ upBit.py
@@ -0,0 +1,47 @@
+import hashlib
+import time
+from urllib.parse import urlencode
+import jwt
+import requests
+import uuid
+from core import get_secret
+
+ACCESS_KEY = get_secret("ACCESS_KEY")
+SECRET_KEY = get_secret("SECRET_KEY")
+payload = {
+    'access_key': ACCESS_KEY,
+    'nonce': str(uuid.uuid4()),
+}
+
+
+def balance_check():
+    url = "https://api.upbit.com/v1/accounts"
+    jwt_token = jwt.encode(payload, SECRET_KEY)
+    authorize_token = 'Bearer {}'.format(jwt_token)
+    headers = {"Authorization": authorize_token}
+    res = requests.get(url, headers=headers)
+    print(res.json())
+
+
+def get_trade_price(market):
+    url = "https://api.upbit.com/v1/candles/days"
+    coin_name = get_all_coin()
+    market = coin_name[market]
+    querystring = {"market": market, "count": "1"}
+    response = requests.request("GET", url, params=querystring)
+    return response.json()[0]['trade_price']
+
+
+def get_all_coin():
+    coin = {}
+    url = "https://api.upbit.com/v1/market/all"
+    querystring = {"isDetails": "false"}
+    response = requests.request("GET", url, params=querystring)
+    for i in range(len(response.json())):
+        if 'KRW' in response.json()[i]['market']:
+            coin[response.json()[i]['korean_name']] = response.json()[i]['market']
+    return coin
+
+# 테스트용
+# get_trade_price("비트코인")
+# get_all_coin()
